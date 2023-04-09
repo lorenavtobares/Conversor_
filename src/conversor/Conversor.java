5752a1bc324982bfd9d67b161f77bb6e0fc740e0
Lorena <lorenav.tobares@gmail.com>
2023-04-09 19:18:14 -0300

    commitiando

---
+++ src/conversor/Conversor.java
@@ -0,0 +1,31 @@
+package conversor;
+
+import conversor.Formulario.Ventana_Principal;
+
+public class Conversor {
+
+    public static void main(String[] args) {
+        
+      
+        
+        /*Cargando fondo*/
+        
+        Ventana_Principal windor = new Ventana_Principal();
+        windor.setVisible(true);
+        
+        String[] tipoConversor = {"Conversor de monedas", "Conversor de Temperatura" ,"Salir"};
+        //JOptionPane.showOptionDialog(ventana que es null, msj de la ventana, titulo de la ventana,
+        // el boton quedan en 0 si los definimos nostros, tipo de msj JOtionPane.QUESTION_MENSAGE, 
+        //El icono puede quedar null o otro, arreglo, boton que queda seleccionado
+        //El arreglo mencionado debe tener las opciones
+       /* int opcion = JOptionPane.showOptionDialog(null, "Elige el tipo de conversor que desea", "Conversor", 0,
+                JOptionPane.QUESTION_MESSAGE, null, tipoConversor, null); 
+        //muestra la ventana del conversor con sus valores y guarda el resultado en la variable opcion
+        System.out.println(opcion=leer.next();*/
+       
+        
+    
+        
+    }
+
+}
